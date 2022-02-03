36c616721e2b27fa25494864d383122f5d3f156c
syedahalam <syedahalam@yahoo.com>
2022-02-03 14:28:06 -0500

    completed sticky

---
+++ sticky/script.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector('.nav');
+window.addEventListener('scroll', fixNav);
+
+function fixNav() {
+	// console.log(window.scrollY, nav.offsetHeight);
+	if (window.scrollY > nav.offsetHeight + 250) {
+		nav.classList.add('active');
+	} else {
+		nav.classList.remove('active');
+	}
+}
